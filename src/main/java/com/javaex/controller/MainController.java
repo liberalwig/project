9106ee211fddb340d757f38c31d37cd899ee47a4
been <been@192.168.0.15>
2022-03-03 11:51:49 +0900

    송

--- src/main/java/com/javaex/controller/MainController.java
+++ src/main/java/com/javaex/controller/MainController.java
@@ -2,17 +2,20 @@ package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-	@Controller
-	public class MainController {
+@Controller
+public class MainController {
 
-		//블로그-메인	
-			@RequestMapping(value = "/main", method = { RequestMethod.GET, RequestMethod.POST })
-			public String Main() {
-				
-				return "song/index";
-			}
+		@RequestMapping("main")
+		public String Main() {
+			
+			return "song/index";
+		}
 
-	}	
+		@RequestMapping("joinForm")
+		public String joinForm() {
+			
+			return "song/joinForm";
+		}
+}	
 	
