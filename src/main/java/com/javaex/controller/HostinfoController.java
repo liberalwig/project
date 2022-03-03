bda4e988522bccd6d694aa6619b09fa1c8f3657f
한상선 <hs94122@naver.com>
2022-03-03 09:49:05 +0900

    한상

---
+++ src/main/java/com/javaex/controller/HostinfoController.java
@@ -0,0 +1,24 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/hostinfo", method = { RequestMethod.GET, RequestMethod.POST })
+public class HostinfoController {
+	
+	@RequestMapping(value = "/hostid", method = { RequestMethod.GET, RequestMethod.POST })
+	public String hostinfo() {
+		System.out.println("[hostinfoController.hostinfo()]");
+		
+		return "/han/hostinfo";
+	}
+	
+	@RequestMapping(value = "/booking", method = { RequestMethod.GET, RequestMethod.POST })
+	public String booking() {
+		System.out.println("[hostinfoController.booking()]");
+		
+		return "/han/booking";
+	}
+}
