827e29f55fab69ee18d43fa9124124ec90683814
최승은 <liberalfilm@gmail.com>
2022-03-11 09:47:57 +0900

    2022-03-11(목) 09:47

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -2,10 +2,16 @@ package com.javaex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.service.UserService;
+import com.javaex.vo.UserVo;
 
 @Controller
 @RequestMapping(value = "/user", method = { RequestMethod.GET, RequestMethod.POST })
@@ -19,8 +25,35 @@ public class UserController {
    public String UserModifyForm() {
       System.out.println("[ UserController > userModifyForm() ]");
       
-      return "/choe/userModifyForm";
+      return "choe/userModifyForm";
    }
+   
+   
+   
+	// 유저_2> 유저 정보수정 시 프로필사진 업데이트
+	@ResponseBody
+	@RequestMapping("/profileUpdate")
+	public String profileUpdate(@PathVariable("usersNo") int usersNo,
+							   @RequestParam("file") MultipartFile file,
+							   @ModelAttribute UserVo userVo) {
+		System.out.println("UserController > profilieUpdate()");
+				
+		//유저 사진 update하기
+		
+		userVo.setUsersNo(usersNo);
+		
+		userService.profileUpdate(file, userVo);
+		
+		return "redirect:/";
+	}	
+   
+   
+   
+   
+   
+   
+   
+   
    
    // 유저_2> 찜
    @RequestMapping(value="/heart")
--- src/main/java/com/javaex/service/UserService.java
+++ src/main/java/com/javaex/service/UserService.java
@@ -1,22 +1,53 @@
 package com.javaex.service;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.dao.UserDao;
+import com.javaex.vo.UserVo;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserDao userDao;
+	
+	//유저 정보수정 시 프로필사진 업데이트
+	public void profileUpdate(MultipartFile file, UserVo userVo) {
+		System.out.println("UserService > profileUpdate()");
+		//내 컴퓨터 저장경로
+		String saveDir = "/C:/javaStudy/workspace_web/project/webapp/assets/upload/";
+		// 원본파일이름
+		String orgName = file.getOriginalFilename();
+		// 확장자
+		String exName = orgName.substring(orgName.lastIndexOf("."));
+		// 저장파일이름
+		String saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;
+		// 파일패스 생성
+		String filePath = saveDir + saveName;
+		// 파일 저장
+		try {
+			byte[] fileData = file.getBytes();
+			OutputStream out = new FileOutputStream(filePath);
+			BufferedOutputStream bout = new BufferedOutputStream(out);
 
-	// 유저_1> 유저 수정 폼
-	@RequestMapping("/hostModifyForm")
-	public String userModifyForm() {
-		System.out.println("[ UserController > userModifyForm() ]");
-
-		return "/choe/hostModifyForm";
+			bout.write(fileData);
+			bout.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//DB 업데이트
+		userVo.setPath(saveName);
+		userDao.profileUpdate(userVo);
 	}
+	
+	
 }
